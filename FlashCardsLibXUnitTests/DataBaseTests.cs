feca3bd5a7870e2310c968f6b192ea6d171b17bd
maubach <maubach@DE-ADN-3CWB2T3>
2022-10-25 15:03:01 +0200

    Pulling changes from main

--- FlashCardsLibXUnitTests/DataBaseTests.cs
+++ FlashCardsLibXUnitTests/DataBaseTests.cs
@@ -1,59 +1,19 @@
-using System.Data.SqlClient;
-using System.Linq;
-
+﻿using FlashCardsLib.Database;
+using System.Data;
 
 namespace FlashCardsLibXUnitTests
 {
-    public class DataBaseTests
+    public class DatabaseTests
     {
         [Fact]
-        public void ConnectAndRequest()
+        public void ConnectToDatabase()
         {
-            SqlConnection cnn = new SqlConnection(connectionString);
-            cnn.Open();
-            cnn.Close();
-            //var output = cnn.Query
-            
+            DatabaseHandler handler = new DatabaseHandler();
+            handler.Connection.Open();
 
+            Assert.Equal(ConnectionState.Open, handler.Connection.State);
 
-
-                try
-            {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-
-                builder.DataSource = "<your_server.database.windows.net>";
-                builder.UserID = "<your_username>";
-                builder.Password = "<your_password>";
-                builder.InitialCatalog = "<your_database>";
-
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-
-                    connection.Open();
-
-                    String sql = "SELECT name, collation_name FROM sys.databases";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            Console.WriteLine("\nDone. Press enter.");
-            Console.ReadLine();
+            handler.Connection.Close();
         }
-
     }
 }
