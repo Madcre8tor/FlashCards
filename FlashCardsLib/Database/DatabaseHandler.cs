4813de8d614ab7798d998f1ffa37e72a3a5c0514
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-10-25 11:41:53 +0200

    Update DatabaseHandler.cs: Adjustments regarding to database information now being retrieved from user env variables rather than from a .json file

--- FlashCardsLib/Database/DatabaseHandler.cs
+++ FlashCardsLib/Database/DatabaseHandler.cs
@@ -5,17 +5,11 @@ namespace FlashCardsLib.Database
 {
     public class DatabaseHandler
     {
-        static readonly string connectionString;
-
-        static DatabaseHandler()
-        {
-            string jsonString = File.ReadAllText("database.json");
-            DatabaseInfo? dbInfo = JsonConvert.DeserializeObject<DatabaseInfo>(jsonString);
-
-            if (dbInfo == null) throw new NullReferenceException("Could not deserialize database.json file");
-
-            connectionString = $"Data Source={dbInfo.Host};Initial Catalog={dbInfo.Database}; User ID={dbInfo.User}; Password={dbInfo.Password}";
-        }
+        static readonly string connectionString =
+            @$"Data Source={Environment.GetEnvironmentVariable("FLASHCARDS_HOST", EnvironmentVariableTarget.User)};
+            Initial Catalog={Environment.GetEnvironmentVariable("FLASHCARDS_DATABASE", EnvironmentVariableTarget.User)};
+            User ID={Environment.GetEnvironmentVariable("FLASHCARDS_USER", EnvironmentVariableTarget.User)};
+            Password={Environment.GetEnvironmentVariable("FLASHCARDS_PASSWORD", EnvironmentVariableTarget.User)}";
 
         public SqlConnection Connection { get; }
 
