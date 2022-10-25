1387aa2746b822c97670b6f999b92e21408b0c63
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-10-25 11:23:38 +0200

    Create DatabaseInfo.cs (Object for deserialization of .json file storing the database information)

---
+++ FlashCardsLib/Database/DatabaseInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashCardsLib.Database
+{
+    public class DatabaseInfo
+    {
+        public string Host { get; set; }
+        public string Database { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
