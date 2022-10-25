54f484a169da5d524b48fcc2b85f1ca14fc3ec66
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-10-25 12:59:49 +0200

    Update DatabaseHandler.cs: Use the SqlConnectionStringBuilder class rather than building the connection string manually

--- FlashCardsLib/Database/DatabaseHandler.cs
+++ FlashCardsLib/Database/DatabaseHandler.cs
@@ -5,10 +5,13 @@ namespace FlashCardsLib.Database
     public class DatabaseHandler
     {
         static readonly string connectionString =
-            @$"Data Source={Environment.GetEnvironmentVariable("FLASHCARDS_HOST", EnvironmentVariableTarget.User)};
-            Initial Catalog={Environment.GetEnvironmentVariable("FLASHCARDS_DATABASE", EnvironmentVariableTarget.User)};
-            User ID={Environment.GetEnvironmentVariable("FLASHCARDS_USER", EnvironmentVariableTarget.User)};
-            Password={Environment.GetEnvironmentVariable("FLASHCARDS_PASSWORD", EnvironmentVariableTarget.User)}";
+            new SqlConnectionStringBuilder()
+            {
+                DataSource = Environment.GetEnvironmentVariable("FLASHCARDS_HOST", EnvironmentVariableTarget.User),
+                InitialCatalog = Environment.GetEnvironmentVariable("FLASHCARDS_DATABASE", EnvironmentVariableTarget.User),
+                UserID = Environment.GetEnvironmentVariable("FLASHCARDS_USER", EnvironmentVariableTarget.User),
+                Password = Environment.GetEnvironmentVariable("FLASHCARDS_PASSWORD", EnvironmentVariableTarget.User)
+            }.ConnectionString;
 
         public SqlConnection Connection { get; }
 
