1fdea3e97b3ff12acfaa515dc387436e45f38ff0
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-10-25 11:21:17 +0200

    Create DatabaseHandler.cs

---
+++ FlashCardsLib/Database/DatabaseHandler.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace FlashCardsLib.Database
+{
+    public class DatabaseHandler
+    {
+        static readonly string connectionString;
+
+        static DatabaseHandler()
+        {
+            string jsonString = File.ReadAllText("database.json");
+            DatabaseInfo? dbInfo = JsonConvert.DeserializeObject<DatabaseInfo>(jsonString);
+
+            if (dbInfo == null) throw new NullReferenceException("Could not deserialize database.json file");
+
+            connectionString = $"Data Source={dbInfo.Host};Initial Catalog={dbInfo.Database}; User ID={dbInfo.User}; Password={dbInfo.Password}";
+        }
+
+        public SqlConnection Connection { get; }
+
+        public DatabaseHandler()
+        {
+            Connection = new SqlConnection(connectionString);
+        }
+    }
+}
