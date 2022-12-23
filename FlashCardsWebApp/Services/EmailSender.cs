b5b6e638d270cbeba67b7274e679643472e89af1
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-12-23 13:09:42 +0100

    Create EmailSender.cs

---
+++ FlashCardsWebApp/Services/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace FlashCardsWebApp.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger _logger;
+
+        public AuthMessageSenderOptions Options { get; set; }
+
+        public EmailSender(ILogger<EmailSender> logger, IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            _logger = logger;
+
+            Options = optionsAccessor.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+            await Execute(Options.SendGridKey, subject, htmlMessage, email);
+        }
+
+        public async Task Execute(string apiKey, string subject, string htmlMessage, string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("noreply@baeni.de"),
+                Subject = subject,
+                PlainTextContent = htmlMessage,
+                HtmlContent = htmlMessage
+            };
+            msg.AddTo(new EmailAddress(email));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            _logger.LogInformation(response.IsSuccessStatusCode
+                                   ? $"Email to {email} queued successfully!"
+                                   : $"Failure Email to {email}");
+        }
+    }
+}
