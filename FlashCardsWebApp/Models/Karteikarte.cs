c6d8548a43d183a2702f17fbaee6b925a4a7bafe
maubach <maubach@DE-ADN-3CWB2T3>
2022-12-23 11:32:06 +0100

    Reverted (deleted) faulty KarteikartenTabelle.cs

---
+++ FlashCardsWebApp/Models/Karteikarte.cs
@@ -0,0 +1,16 @@
+﻿namespace FlashCardsWebApp.Models
+{
+    public class Karteikarte
+    {
+        public int Id { get; set; }
+        public string Frage { get; set; }
+        public string Antwort { get; set; }
+
+        public Karteikarte(int id, string frage, string antwort)
+        {
+            Id = id;
+            Frage = frage;
+            Antwort = antwort;
+        }
+    }
+}
