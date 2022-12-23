ab9a56ea8110546f987271d24ba0834c77acfb9b
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2022-12-23 11:27:06 +0100

    Update Program.cs: Set connection string to IdentityConnection

--- FlashCardsWebApp/Program.cs
+++ FlashCardsWebApp/Program.cs
@@ -11,7 +11,7 @@ namespace FlashCardsWebApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("IdentityConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
