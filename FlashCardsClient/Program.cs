52b7a81adbba15d709157ba6378cdce4eb606a87
maubach <maubach@DE-ADN-3CWB2T3>
2023-02-07 14:55:09 +0100

    Added first Initializiation lines. Refactoring still needed.

--- FlashCardsClient/Program.cs
+++ FlashCardsClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace FlashCardsClient
 {
@@ -11,7 +13,7 @@ namespace FlashCardsClient
         {
 
             Console.WriteLine("Please enter the IP-Address to connect to");
-            Console.WriteLine("Or press Enter with no input to default to IP-Address of VM Server: ");
+            Console.WriteLine("Or press Enter with no input to use default IP-Address of VM Server: ");
             string ipString = Console.ReadLine();
             if (string.IsNullOrEmpty(ipString)) {
                 ipString = defaultIPString;
@@ -19,7 +21,19 @@ namespace FlashCardsClient
             IPAddress ipAddress = IPAddress.Parse(ipString);
 
             Console.WriteLine("defaultPort is 5000");
-            var ipEndPoint = new IPEndPoint(ipAddress, 5000);
+            IPEndPoint ipEndPoint = new(ipAddress, defaultPort);
+
+            Console.WriteLine("Establishing connection...");
+
+            using TcpClient client = new();
+            await client.ConnectAsync(ipEndPoint);
+            await using NetworkStream stream = client.GetStream();
+
+            var buffer = new byte[1_024];
+            int received = await stream.ReadAsync(buffer);
+
+            var message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Message received: \"{message}\"");
 
 
 
